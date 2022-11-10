d1ae085baa4cc11c3b0dc8e387e07bd3e66302ed
Suraj Doss <suraj_sebastien@yahoo.com>
2022-11-10 08:42:55 +0530

    Update 595. Big Countries.sql

--- 595. Big Countries.sql
+++ 595. Big Countries.sql
@@ -1,14 +1,14 @@
-+-------------+---------+
-| Column Name | Type    |
-+-------------+---------+
-| name        | varchar |
-| continent   | varchar |
-| area        | int     |
-| population  | int     |
-| gdp         | int     |
-+-------------+---------+
-name is the primary key column for this table.
-Each row of this table gives information about the name of a country, the continent to which it belongs, its area, the population, and its GDP value.
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | name        | varchar |
+-- | continent   | varchar |
+-- | area        | int     |
+-- | population  | int     |
+-- | gdp         | int     |
+-- +-------------+---------+
+-- name is the primary key column for this table.
+-- Each row of this table gives information about the name of a country, the continent to which it belongs, its area, the population, and its GDP value.
 
 
 

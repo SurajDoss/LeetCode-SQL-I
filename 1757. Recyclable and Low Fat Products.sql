df041d31b9ddbd284d9f101662885786e15ac57a
Suraj Doss <suraj_sebastien@yahoo.com>
2022-11-10 08:42:38 +0530

    Update 1757. Recyclable and Low Fat Products.sql

--- 1757. Recyclable and Low Fat Products.sql
+++ 1757. Recyclable and Low Fat Products.sql
@@ -1,36 +1,36 @@
-+-------------+---------+
-| Column Name | Type    |
-+-------------+---------+
-| product_id  | int     |
-| low_fats    | enum    |
-| recyclable  | enum    |
-+-------------+---------+
-product_id is the primary key for this table.
-low_fats is an ENUM of type ('Y', 'N') where 'Y' means this product is low fat and 'N' means it is not.
-recyclable is an ENUM of types ('Y', 'N') where 'Y' means this product is recyclable and 'N' means it is not.
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | product_id  | int     |
+-- | low_fats    | enum    |
+-- | recyclable  | enum    |
+-- +-------------+---------+
+-- product_id is the primary key for this table.
+-- low_fats is an ENUM of type ('Y', 'N') where 'Y' means this product is low fat and 'N' means it is not.
+-- recyclable is an ENUM of types ('Y', 'N') where 'Y' means this product is recyclable and 'N' means it is not.
 
 
 
 
-Input: 
-Products table:
-+-------------+----------+------------+
-| product_id  | low_fats | recyclable |
-+-------------+----------+------------+
-| 0           | Y        | N          |
-| 1           | Y        | Y          |
-| 2           | N        | Y          |
-| 3           | Y        | Y          |
-| 4           | N        | N          |
-+-------------+----------+------------+
-Output: 
-+-------------+
-| product_id  |
-+-------------+
-| 1           |
-| 3           |
-+-------------+
-Explanation: Only products 1 and 3 are both low fat and recyclable.
+-- Input: 
+-- Products table:
+-- +-------------+----------+------------+
+-- | product_id  | low_fats | recyclable |
+-- +-------------+----------+------------+
+-- | 0           | Y        | N          |
+-- | 1           | Y        | Y          |
+-- | 2           | N        | Y          |
+-- | 3           | Y        | Y          |
+-- | 4           | N        | N          |
+-- +-------------+----------+------------+
+-- Output: 
+-- +-------------+
+-- | product_id  |
+-- +-------------+
+-- | 1           |
+-- | 3           |
+-- +-------------+
+-- Explanation: Only products 1 and 3 are both low fat and recyclable.
 
 
 
